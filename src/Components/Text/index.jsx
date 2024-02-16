9c80b782b8cb63c0e69934a7c22d5ebffaf259b4
Ramkumar9869 <ramkumarjp9869@gmail.com>
2024-02-16 22:05:16 +0530

    deployed code on github pages

---
+++ src/Components/Text/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/header";
+
+function Text() {
+  return (
+    <div className="container">
+      <Header />
+      <div className="d-flex flex-column justify-content-center text-center">
+        <h1>HEllo JavaScript</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Text;
